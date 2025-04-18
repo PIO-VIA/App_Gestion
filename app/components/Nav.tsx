6fa147b11b362ce20dd1987c103b8c2569aad727
PIO <piodjiele@gmail.com>
2025-04-18 16:25:12 +0100

    mode light et dark

---
+++ app/components/Nav.tsx
@@ -0,0 +1,21 @@
+"use-client";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "@/public/logo.svg";
+import { ThemeToggle } from "./ThemeToggle";
+
+export default function Nav() {
+  return (
+    <nav className = "max-w-[1200px] w-full mx-auto h-[80px] flex items-center justify-between p-5 border-b border-gray-300">
+      <div>
+        <Link href ='/'>
+          <Image width={30} height={30} src={logo} alt="logo de l'application"
+          className="w-12 h-12" />
+        </Link>
+      </div>
+      <div className="flex items-center">
+        <ThemeToggle />
+      </div>
+    </nav>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Nav from "./components/Nav"; 
+import { ThemeProvider } from "./components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+         <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+        <Nav/>
         {children}
+        </ThemeProvider>
       </body>
     </html>
   );
