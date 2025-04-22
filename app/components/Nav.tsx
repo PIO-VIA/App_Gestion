0f4c6c07ebe1c8d296dd7f18c161767c70378799
PIO <piodjiele@gmail.com>
2025-04-22 11:35:42 +0100

    authentification github ok

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -6,7 +6,7 @@ import { ThemeToggle } from "./ThemeToggle";
 
 export default function Nav() {
   return (
-    <nav className = "max-w-[1200px] w-full mx-auto h-[80px] flex items-center justify-between p-5 border-b border-gray-300">
+    <nav className = " w-full mx-auto h-[80px] flex items-center justify-between p-5 border-b border-gray-300">
       <div>
         <Link href ='/'>
           <Image width={30} height={30} src={logo} alt="logo de l'application"
---
+++ lib/AuthOptions.ts
@@ -0,0 +1,20 @@
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET!,
+};
